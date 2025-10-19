a3873fd06024f68d90e64ca12eb378f4b3303c76
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-19 08:16:57 +0600

    Add pricingCard

---
+++ src/Components/PricingCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PricingCard = ({pricing}) => {
+    console.log(pricing)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PricingCard;
--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -1,11 +1,19 @@
 import React, { use } from 'react';
+import PricingCard from './PricingCard';
 
 const PricingOptions = ({pricingPromise}) => {
     const promiseData = use(pricingPromise);
-    console.log(promiseData)
+    // console.log(promiseData)
     return (
         <div>
-            
+            <h2 className='text-3xl'>Get our membership</h2>
+            <div>
+                {
+                    promiseData.map((pricing => <PricingCard
+                    key={pricing.id}
+                         pricing={pricing}></PricingCard>))
+                }
+            </div>
         </div>
     );
 };
