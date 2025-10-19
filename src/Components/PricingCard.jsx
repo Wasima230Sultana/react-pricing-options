25d23077f33583cd44be0354104fb012a2118521
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-19 20:40:40 +0600

    Add button

--- src/Components/PricingCard.jsx
+++ src/Components/PricingCard.jsx
@@ -6,14 +6,14 @@ const PricingCard = ({pricing}) => {
     console.log(pricing)
     return (
      
-        <div className='border bg-amber-600 rounded-lg p-5'>
+        <div className='border bg-amber-600 rounded-lg p-5 flex flex-col'>
             {/* card header  */}
             <div >
                 <h2 className='text-7xl'>{name}</h2>
                 <h4 className='text-3xl'>{price}</h4>
             </div>
             {/* card body  */}
-            <div className='bg-amber-400 p-4 rounded-2xl mt-10'>
+            <div className='bg-amber-400 p-4 rounded-2xl mt-10 flex-1'>
                 <p>{duration}</p>
                 {
                     features.map(((feature,index) => <PricingFeatures
@@ -21,6 +21,7 @@ const PricingCard = ({pricing}) => {
                     ></PricingFeatures>))
                 }
             </div>
+            <button className="btn w-full mt-5 rounded-2xl bg-black text-white">Subscribe</button>
         </div>
 
        
--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -8,7 +8,7 @@ const PricingOptions = ({ pricingPromise }) => {
     return (
         <div>
             <h2 className='text-3xl'>Get our membership</h2>
-            <div className=' grid grid-cols-1 md:grid-cols-3 gap-7'>
+            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
                 {
                     promiseData.map((pricing => <PricingCard
                         key={pricing.id}
