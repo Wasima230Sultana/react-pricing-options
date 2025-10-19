e0688de1830b38f76b4671b6a57a357a1be2ca65
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-19 20:53:45 +0600

    Add daisypricing

--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from './PricingCard';
+import DaisyPricing from './DaisyPricing';
 
 const PricingOptions = ({ pricingPromise }) => {
     const promiseData = use(pricingPromise);
@@ -9,11 +10,17 @@ const PricingOptions = ({ pricingPromise }) => {
         <div>
             <h2 className='text-3xl'>Get our membership</h2>
             <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
-                {
+                {/* {
                     promiseData.map((pricing => <PricingCard
                         key={pricing.id}
                         pricing={pricing}
                     ></PricingCard>))
+                } */}
+                {
+                    promiseData.map((pricing) => <DaisyPricing
+                    key={pricing.id}
+                    pricing = {pricing}
+                    ></DaisyPricing>)
                 }
             </div>
         </div>
