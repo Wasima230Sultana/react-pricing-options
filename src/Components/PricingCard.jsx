ec69740b4285d177676cf459cfeb3c4f0a6b2e74
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-19 08:38:20 +0600

    Add pricing features

--- src/Components/PricingCard.jsx
+++ src/Components/PricingCard.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import PricingFeatures from './PricingFeatures';
 
 const PricingCard = ({pricing}) => {
+    const {name, price, duration, features} = pricing;
     console.log(pricing)
     return (
-        <div>
-            
+     
+        <div className='border bg-amber-600 rounded-lg p-5'>
+            {/* card header  */}
+            <div >
+                <h2 className='text-7xl'>{name}</h2>
+                <h4 className='text-3xl'>{price}</h4>
+            </div>
+            {/* card body  */}
+            <div className='bg-amber-400 p-4 rounded-2xl mt-10'>
+                <p>{duration}</p>
+                {
+                    features.map(((feature,index) => <PricingFeatures
+                    key={index} feature={feature}
+                    ></PricingFeatures>))
+                }
+            </div>
         </div>
+
+       
     );
 };
 
--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -1,17 +1,19 @@
 import React, { use } from 'react';
 import PricingCard from './PricingCard';
 
-const PricingOptions = ({pricingPromise}) => {
+const PricingOptions = ({ pricingPromise }) => {
     const promiseData = use(pricingPromise);
+
     // console.log(promiseData)
     return (
         <div>
             <h2 className='text-3xl'>Get our membership</h2>
-            <div>
+            <div className=' grid grid-cols-1 md:grid-cols-3 gap-7'>
                 {
                     promiseData.map((pricing => <PricingCard
-                    key={pricing.id}
-                         pricing={pricing}></PricingCard>))
+                        key={pricing.id}
+                        pricing={pricing}
+                    ></PricingCard>))
                 }
             </div>
         </div>
